2a0bbd44f4383242da7b3a8d00e0b7a40123f9b9
Pedro Requiao <alonsofts@gmail.com>
2024-02-20 01:44:07 -0500

    add: api connection setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
-import { router } from './src/routes'
+import { queryClient } from './lib/react-query'
+import { router } from './routes'
 
 export function App() {
   return (
@@ -15,7 +17,9 @@ export function App() {
 
         <Toaster richColors />
 
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
