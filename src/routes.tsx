9e1138c394f3fca4d0dd14f5ecf981a31d0b57f3
Pedro Requiao <alonsofts@gmail.com>
2024-02-20 15:44:43 -0500

    add: sign-up api connection

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,8 +5,9 @@ import { AuthLayout } from '@/pages/_layout/auth'
 import { NotFound } from '@/pages/404'
 import { Dashboard } from '@/pages/app/dashboard/dashboard'
 import { Orders } from '@/pages/app/orders/orders'
-import { SignIn } from '@/pages/auth/sign-in'
 import { SignUp } from '@/pages/auth/sign-up'
+
+import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
   {
