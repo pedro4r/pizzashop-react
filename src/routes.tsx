f38c5dbecd6ff12584d713aea61f1fdc19f6f9c0
Pedro Requiao <alonsofts@gmail.com>
2024-02-23 03:50:06 -0500

    add: loading skeleton to orders list and orders details

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from '@/pages/_layout/app'
-import { AuthLayout } from '@/pages/_layout/auth'
-import { NotFound } from '@/pages/404'
-import { Dashboard } from '@/pages/app/dashboard/dashboard'
-import { Orders } from '@/pages/app/orders/orders'
-import { SignUp } from '@/pages/auth/sign-up'
-
+import { AppLayout } from './pages/_layout/app'
+import { AuthLayout } from './pages/_layout/auth'
+import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -38,5 +38,9 @@ export const router = createBrowserRouter([
         element: <SignUp />,
       },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
